f087cb5cd5fbc5f2cda31c67da305cb4c9704e74
syedcreation133-ai <syedcreation133@gmail.com>
2026-03-20 23:23:27 +0500

    Refactor LSTM text generator implementation
    Refactor text generation logic and improve model loading.

--- app.py
+++ app.py
@@ -1,43 +1,40 @@
-import pickle
 import streamlit as st
-from tensorflow.keras.models import load_model
+import numpy as np
+import pickle
+from tensorflow import keras
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-st.title('LSTM Text Generator')
-
-seed_text = st.text_input('Seed text', value='romeo and juliet')
-num_words = st.slider('Number of words to generate', min_value=5, max_value=50, value=20)
+st.title("LSTM Text Generator")
+st.write("Shakespeare style text generation")
 
 @st.cache_resource
-def load_resources():
-    model = load_model('lstm_model.h5')
+def load_model():
+    model = keras.models.load_model('lstm_model.h5', compile=False)
     with open('tokenizer.pkl', 'rb') as f:
         tokenizer = pickle.load(f)
     with open('maxlen.pkl', 'rb') as f:
-        maxlen = pickle.load(f)
-    return model, tokenizer, maxlen
+        max_len = pickle.load(f)
+    return model, tokenizer, max_len
 
-model, tokenizer, maxlen = load_resources()
+model, tokenizer, max_len = load_model()
 
-if st.button('Generate'):
-    text = seed_text.lower()
-    output = text
-
-    for _ in range(num_words):
-        token_list = tokenizer.texts_to_sequences([text])[0]
-        token_list = pad_sequences([token_list], maxlen=maxlen-1, padding='pre')
-        predicted = model.predict(token_list, verbose=0)
-        predicted_word_index = predicted.argmax(axis=-1)[0]
-
+def generate_text(seed_text, next_words):
+    result = seed_text
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([result])[0]
+        token_list = pad_sequences([token_list], maxlen=max_len-1, padding='pre')
+        predicted = np.argmax(model.predict(token_list, verbose=0), axis=-1)
         for word, index in tokenizer.word_index.items():
-            if index == predicted_word_index:
-                predicted_word = word
+            if index == predicted:
+                result += " " + word
                 break
-        else:
-            predicted_word = ''
+    return result
 
-        output += ' ' + predicted_word
-        text += ' ' + predicted_word
+seed = st.text_input("Enter seed text:", "to be or not")
+words = st.slider("Words to generate:", 5, 50, 20)
 
-    st.subheader('Generated Text')
-    st.write(output)
+if st.button("Generate Text"):
+    with st.spinner("Generating..."):
+        output = generate_text(seed.lower(), words)
+        st.success("Generated Text:")
+        st.write(output)
